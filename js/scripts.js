1cd8af19827c7d0d259596d067d74be52ac97da3
Christine Fruehling <alfomila@gmail.com>
2016-02-07 17:51:23 -0800

    Add div and  ul with Event Listners and with new functions, placed jQuery(document).ready(function()at the top of scrips.js; show new Title and Header 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,21 @@
-jQuery("h1").click(function() {
-  alert("This is a header.");
-});
+jQuery(document).ready(function(){
+  jQuery("h1").click(function() {
+    alert("This is a header.");
+  });
 
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
+  });
 
-jQuery("img").click(function() {
-  alert("This is an image.");
-});
+  jQuery("img").click(function() {
+    alert("This is an image.");
+  });
 
-jQuery("div").click(function() {
-  alert("This is special.");
+  jQuery("div").click(function() {
+    alert("This is special.");
+  });
+
+  jQuery("ul").click(function() {
+    alert("This is ice cream.");
+  });
 });
