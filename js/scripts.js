ad98efac64c8c224bb56d76633bb98fcbd1a5e4c
Christine Fruehling <alfomila@gmail.com>
2016-02-07 16:54:58 -0800

    Add an image, jQuery-Practice.html, js/js/scripts.js.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+jQuery("h1").click(function() {
+  alert("This is a header.");
+});
+
+jQuery("p").click(function() {
+  alert("This is a paragraph.");
+});
+
+jQuery("img").click(function() {
+  alert("This is an image.");
+});
+
+jQuery("div").click(function() {
+  alert("This is special.");
+});
